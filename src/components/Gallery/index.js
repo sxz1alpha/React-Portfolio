21ba16a1c833dc987aa24bb4c686475dc1ca173c
UCDT <adrian.j.ulibarri@gmail.com>
2021-08-26 13:25:06 -0600

    added work index.js

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Gallery() {
     return (
         <section>
-
+            <h1>Section starter code</h1>
+            <p>Here are examples of my work.</p>
+            
         </section>
     )
 }
---
+++ src/components/Work/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const WorkList = ({ tab }) => {
+
+    return (
+        <div>
+            <div className="flex-row">
+                <img src="" alt="" className="" key=""></img>
+            </div>
+        </div>
+    ) 
+}
+
+export default WorkList;
